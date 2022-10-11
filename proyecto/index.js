5df314a80ff3bed9ba67b012ad02b432cf8ceb7c
Gueicha11 <112427484+Gueicha11@users.noreply.github.com>
2022-10-11 12:11:33 -0300

    2.6 Ejercicio de JavaScript

---
+++ proyecto/index.js
@@ -0,0 +1,19 @@
+let nombre = "Agustin";
+let edad = 20;
+const lista = [nombre, edad];
+console.log(lista)
+
+if (true) {
+    console.log("Soy desarrollador")
+}
+else {
+console.log("No soy desarrollador")
+}
+
+const libro = {
+titulo: "Padre rico,padre pobre",
+Autor: "Robert Kiyosaki, Sharon Lechter",
+fecha: new Date(1997),
+idioma: "ingles",
+url: "https://www.unpa.edu.mx/~blopez/algunosLibros/Padre-Rico-Padre-Pobre.pdf",
+}
